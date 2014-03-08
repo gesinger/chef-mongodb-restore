a11af7d4a872610e7ccb24d1429626a4d6ae722a
Garrett Singer <gesinger@gmail.com>
2014-03-08 11:02:26 -0500

    Added metadata.rb

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'mongodb-restore'
+maintainer       'Garrett Singer'
+maintainer_email 'gesinger@gmail.com'
+license          'MIT'
+description      'Restores a mongodb database'
+version          '0.0.1'
+
+recipe 'mongodb-restore', 'Restores a mongodb databse'
