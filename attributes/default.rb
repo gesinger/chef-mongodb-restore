4e8455145992abc8053a716daff0ced8945d21a8
Garrett Singer <gesinger@gmail.com>
2014-03-08 11:22:26 -0500

    Added default recipe and attribute

---
+++ attributes/default.rb
@@ -0,0 +1,9 @@
+default[:mongodb_restore][:host]          = nil
+default[:mongodb_restore][:port]          = nil
+default[:mongodb_restore][:db]            = nil
+default[:mongodb_restore][:dbpath]        = nil
+default[:mongodb_restore][:collection]    = nil
+default[:mongodb_restore][:file]          = nil
+default[:mongodb_restore][:use_journal]   = nil
+default[:mongodb_restore][:ignore_blanks] = nil
+default[:mongodb_restore][:tail]          = nil
---
+++ recipes/default.rb
@@ -0,0 +1,45 @@
+# Cookbook Name:: mongodb-restore
+# Recipe:: default
+
+host          = node[:mongodb_restore][:host]
+port          = node[:mongodb_restore][:port]
+db            = node[:mongodb_restore][:db]
+dbpath        = node[:mongodb_restore][:dbpath]
+collection    = node[:mongodb_restore][:collection]
+file          = node[:mongodb_restore][:file]
+use_journal   = node[:mongodb_restore][:use_journal]
+ignore_blanks = node[:mongodb_restore][:ignore_blanks]
+tail          = node[:mongodb_restore][:tail]
+
+restore_script = "mongorestore"
+if host
+  restore_script += " --host #{host}"
+end
+if port
+  restore_script += " --port #{port}"
+end
+if db
+   restore_script += " --db #{db}"
+end
+if dbpath
+   restore_script += " --dbpath #{dbpath}"
+end
+if collection
+   restore_script += " --collection #{collection}"
+end
+if use_journal
+   restore_script += " --journal"
+end
+if ignore_blanks
+   restore_script += " --ignoreBlanks"
+end
+
+restore_script += " --file #{file}"
+
+if tail
+  restore_script += " #{tail}"
+end
+
+execute "run restore script" do
+  command restore_script
+end
